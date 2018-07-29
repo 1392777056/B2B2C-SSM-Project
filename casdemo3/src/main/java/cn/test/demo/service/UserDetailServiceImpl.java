d735b393fd799b19efe4b3ee960256f9dab0b905
ZhangDeZhe <1392777056@qq.com>
2018-07-29 11:01:46 +0800

    spring-security和CAS的整合。

---
+++ casdemo3/src/main/java/cn/test/demo/service/UserDetailServiceImpl.java
@@ -0,0 +1,29 @@
+package cn.test.demo.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create with www.dezhe.com
+ *
+ * @Author 德哲
+ * @Date 2018/7/29 10:39
+ */
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<GrantedAuthority> list = new ArrayList<GrantedAuthority>();
+        SimpleGrantedAuthority role_user = new SimpleGrantedAuthority("ROLE_USER");
+        list.add(role_user);
+
+        return new User(username,"",list);
+    }
+}
